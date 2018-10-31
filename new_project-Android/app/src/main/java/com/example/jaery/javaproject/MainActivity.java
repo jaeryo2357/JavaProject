ba685bf07c73328808dc927ee1c665c5844cd585
jaeryo <jaeryo2357@naver.com>
2018-10-31 16:56:10 +0900

    리스트 뷰 리스너

--- new_project-Android/app/src/main/java/com/example/jaery/javaproject/MainActivity.java
+++ new_project-Android/app/src/main/java/com/example/jaery/javaproject/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -30,20 +31,31 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        WebtoonListAdapter adapter=new WebtoonListAdapter(this);
+        final WebtoonListAdapter adapter=new WebtoonListAdapter(this);
 
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
-        adapter.addItem("신과함께","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+
+
+        ///리스트 추가하는 부분
+        adapter.addItem("신과함께1","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께2","작가2","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께3","작가3","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께4","작가4","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께5","작가5","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께6","작가6","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께7","작가7","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께8","작가8","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        adapter.addItem("신과함께9","작가9","2018.10.2",BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
         ListView listView=findViewById(R.id.listview);
         listView.setAdapter(adapter);
 
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                WebToonItem ClickingItem=adapter.getItem(position);
+                Toast.makeText(MainActivity.this,ClickingItem.getTitle(),Toast.LENGTH_LONG).show();
+            }
+        });
 
 
 
