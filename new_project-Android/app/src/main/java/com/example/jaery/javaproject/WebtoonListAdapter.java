7885f5fc2233e67c050fb93edbbbd2104ce02d5b
jaeryo <jaeryo2357@naver.com>
2018-11-01 09:16:47 +0900

    검색창 둥글게 오류수정

--- new_project-Android/app/src/main/java/com/example/jaery/javaproject/WebtoonListAdapter.java
+++ new_project-Android/app/src/main/java/com/example/jaery/javaproject/WebtoonListAdapter.java
@@ -6,7 +6,7 @@ import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.HorizontalScrollView;
+
 import android.widget.ImageView;
 
 import android.widget.TextView;
