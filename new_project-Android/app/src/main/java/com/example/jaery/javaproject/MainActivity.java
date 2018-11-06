bac9355a2ad5d55c59e983f05e29f9f17d444d23
jaeryo <jaeryo2357@naver.com>
2018-11-06 20:37:37 +0900

    검색창 expend 애니메이션

--- new_project-Android/app/src/main/java/com/example/jaery/javaproject/MainActivity.java
+++ new_project-Android/app/src/main/java/com/example/jaery/javaproject/MainActivity.java
@@ -1,25 +1,120 @@
 package com.example.jaery.javaproject;
 
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    EditText editText;
+    boolean express;
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        express=false;// 처음에는 검색창이 확장되어 있지않음
 
-        final WebtoonListAdapter adapter=new WebtoonListAdapter(this);
+
+        editText=findViewById(R.id.search_webtoon);
+        view=findViewById(R.id.inflate);
+
+        editText.setOnClickListener(new View.OnClickListener(
+
+        ) {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void onClick(View v) {
+
+                if(express)
+                {
+                    /*
+                    RelativeLayout.LayoutParams layoutParams=(RelativeLayout.LayoutParams)editText.getLayoutParams();
+                //    layoutParams.removeRule(RelativeLayout.ALIGN_LEFT);
+                    layoutParams.addRule(RelativeLayout.ALIGN_LEFT,R.id.notinflate);
+                    editText.setLayoutParams(layoutParams);
+                    express=false;
+                    editText.setFocusable(false);
+                    layoutParams=(RelativeLayout.LayoutParams)view.getLayoutParams();
+                    layoutParams.leftMargin=230;
+                    view.setLayoutParams(layoutParams);
+                    e
+*/
+                    editText.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            editText.setFocusableInTouchMode(true);
+                            editText.requestFocus();
+
+                            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                            imm.showSoftInput(editText,0);
+
+                        }
+                    });
+
+                }
+                else{
+                    express = true;
+                                editText.clearFocus();
+                                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) editText.getLayoutParams();
+                                //    layoutParams.removeRule(RelativeLayout.ALIGN_LEFT);
+                                layoutParams.addRule(RelativeLayout.ALIGN_LEFT, R.id.inflate);
+                                editText.setLayoutParams(layoutParams);
+
+                                layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    ValueAnimator animator = ValueAnimator.ofInt(layoutParams.leftMargin, 0);
+                    final RelativeLayout.LayoutParams finalLayoutParams = layoutParams;
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator)
+                        {
+                            finalLayoutParams.leftMargin = (Integer) valueAnimator.getAnimatedValue();
+
+                            view.setLayoutParams(finalLayoutParams);
+                        }
+                    });
+                    animator.setDuration(200);
+                    animator.start();
+                    }
+                }
+        });
+
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    RelativeLayout.LayoutParams layoutParams=(RelativeLayout.LayoutParams)editText.getLayoutParams();
+                    //    layoutParams.removeRule(RelativeLayout.ALIGN_LEFT);
+                    layoutParams.addRule(RelativeLayout.ALIGN_LEFT,R.id.notinflate);
+                    editText.setLayoutParams(layoutParams);
+                    express=false;
+                    editText.setText("");
+                    editText.setFocusable(false);
+                    layoutParams=(RelativeLayout.LayoutParams)view.getLayoutParams();
+                    layoutParams.leftMargin=230;
+                    view.setLayoutParams(layoutParams);
+                }
+            }
+        });
 
 
+/*
+     final WebtoonListAdapter adapter=new WebtoonListAdapter(this);
 
         ///리스트 추가하는 부분
        adapter.addItem(R.layout.webtoon_list_item,"신과함께1","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.drawable.cat_food));
@@ -52,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+*/
 
     }
 }
