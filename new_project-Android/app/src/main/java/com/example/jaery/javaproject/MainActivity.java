4da87e5a3e510d2b71abd7909d29ec7d914b6974
jaeryo <jaeryo2357@naver.com>
2018-11-09 13:51:58 +0900

    UI 다듬기

--- new_project-Android/app/src/main/java/com/example/jaery/javaproject/MainActivity.java
+++ new_project-Android/app/src/main/java/com/example/jaery/javaproject/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.jaery.javaproject;
 
 import android.animation.ValueAnimator;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -32,8 +37,11 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<WebToonItem> myDataset;
+    boolean gone=false;
 
+    int oldScrollY=0;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,95 +75,161 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
 
-                }else{
+                } else {
 
-                express = true;
-                TextView textView=findViewById(R.id.Project_title);
-                textView.setVisibility(View.GONE);
-                editText.clearFocus();
-                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) editText.getLayoutParams();
-                //    layoutParams.removeRule(RelativeLayout.ALIGN_LEFT);
-                layoutParams.addRule(RelativeLayout.ALIGN_LEFT, R.id.inflate);
-                editText.setLayoutParams(layoutParams);
+                    express = true;
+                    TextView textView = findViewById(R.id.Project_title);
+                    textView.setVisibility(View.GONE);
+                    editText.clearFocus();
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) editText.getLayoutParams();
+                    //    layoutParams.removeRule(RelativeLayout.ALIGN_LEFT);
+                    layoutParams.addRule(RelativeLayout.ALIGN_LEFT, R.id.inflate);
+                    editText.setLayoutParams(layoutParams);
 
-                layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                ValueAnimator animator = ValueAnimator.ofInt(layoutParams.leftMargin, 0);
-                final RelativeLayout.LayoutParams finalLayoutParams = layoutParams;
-                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                        finalLayoutParams.leftMargin = (Integer) valueAnimator.getAnimatedValue();
+                    layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    ValueAnimator animator = ValueAnimator.ofInt(layoutParams.leftMargin, 0);
+                    final RelativeLayout.LayoutParams finalLayoutParams = layoutParams;
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            finalLayoutParams.leftMargin = (Integer) valueAnimator.getAnimatedValue();
 
-                        view.setLayoutParams(finalLayoutParams);
-                    }
-                });
-                animator.setDuration(300);
-                animator.start();
+                            view.setLayoutParams(finalLayoutParams);
+                        }
+                    });
+                    animator.setDuration(300);
+                    animator.start();
+                }
             }
-        }
-    });
+        });
 
         editText.setOnFocusChangeListener(new View.OnFocusChangeListener()
 
-    {
-        @Override
-        public void onFocusChange (View v,boolean hasFocus){
-        if (!hasFocus) {
-            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) editText.getLayoutParams();
-            //    layoutParams.removeRule(RelativeLayout.ALIGN_LEFT);
-            layoutParams.addRule(RelativeLayout.ALIGN_LEFT, R.id.notinflate);
-            editText.setLayoutParams(layoutParams);
-            express = false;
-            editText.setText("");
-            editText.setFocusable(false);
-            layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-            layoutParams.leftMargin = 250;
-            view.setLayoutParams(layoutParams);
-        }
-    }
-    });
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) editText.getLayoutParams();
+                    //    layoutParams.removeRule(RelativeLayout.ALIGN_LEFT);
+                    layoutParams.addRule(RelativeLayout.ALIGN_LEFT, R.id.notinflate);
+                    editText.setLayoutParams(layoutParams);
+                    express = false;
+                    editText.setText("");
+                    editText.setFocusable(false);
+                    layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    layoutParams.leftMargin = 250;
+                    view.setLayoutParams(layoutParams);
+                }
+            }
+        });
 
-    mRecyclerView =(RecyclerView)
+        mRecyclerView = (RecyclerView)
 
-    findViewById(R.id.recycle);
+                findViewById(R.id.recycle);
 
-    mLayoutManager=new
+        mLayoutManager = new
 
-    LinearLayoutManager(this);
+                LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-    myDataset =new ArrayList<>();
-    mAdapter =new
+        myDataset = new ArrayList<>();
+        mAdapter = new
 
-    CardViewAdapter(myDataset);
+                CardViewAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
 
         myDataset.add(new
 
-    WebToonItem(R.layout.webtoon_list_item,"신과함께1","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.drawable.webtoon_test)));
+                WebToonItem(R.layout.webtoon_list_item, "신과함께1", "작가1", "2018.10.2", BitmapFactory.decodeResource(getResources(), R.drawable.webtoon_test)));
         myDataset.add(new
 
-    WebToonItem(R.layout.webtoon_list_item,"신과함께1","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.drawable.webtoon_test)));
+                WebToonItem(R.layout.webtoon_list_item, "신과함께1", "작가1", "2018.10.2", BitmapFactory.decodeResource(getResources(), R.drawable.webtoon_test)));
 
         myDataset.add(new
 
-    WebToonItem(R.layout.webtoon_list_item,"신과함께1","작가1","2018.10.2",BitmapFactory.decodeResource(getResources(),R.drawable.webtoon_test)));
+                WebToonItem(R.layout.webtoon_list_item, "신과함께1", "작가1", "2018.10.2", BitmapFactory.decodeResource(getResources(), R.drawable.webtoon_test)));
 
-        mRecyclerView.addOnItemTouchListener(new
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), mRecyclerView, new RecyclerItemClickListener.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(getApplicationContext(), position + "번 째 아이템 클릭", Toast.LENGTH_SHORT).show();
+            }
 
-    RecyclerItemClickListener(getApplicationContext(),mRecyclerView, new RecyclerItemClickListener.OnItemClickListener()
+            @Override
+            public void onLongItemClick(View view, int position) {
+              //  Toast.makeText(getApplicationContext(), position + "번 째 아이템 롱 클릭", Toast.LENGTH_SHORT).show();
+                DialogFragment notice=new NoticeDialogFragment();
+                notice.setCancelable(false);
+                notice.show(getSupportFragmentManager(),"notice");
+            }
+        }));
+/*
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
 
-    {
-        @Override
-        public void onItemClick (View view,int position){
-        Toast.makeText(getApplicationContext(), position + "번 째 아이템 클릭", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if(oldScrollY==0)
+                {
+                    oldScrollY=dy;
+                }else if((oldScrollY-dy)>0)
+                {
+                    if(gone==false) {
+                        RelativeLayout relativeLayout = findViewById(R.id.topbar);
+                        relativeLayout.setVisibility(View.GONE);
+                        gone = true;
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) recyclerView.getLayoutParams();
+                        layoutParams.removeRule(RelativeLayout.BELOW);
+                        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                        recyclerView.setLayoutParams(layoutParams);
+                    }
+                        oldScrollY = dy;
+
+                }else
+                {
+                    if(gone) {
+                        RelativeLayout relativeLayout = findViewById(R.id.topbar);
+                        relativeLayout.setVisibility(View.VISIBLE);
+                        gone=false;
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) recyclerView.getLayoutParams();
+                        layoutParams.addRule(RelativeLayout.BELOW, R.id.topbar);
+                        layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_TOP);
+                        recyclerView.setLayoutParams(layoutParams);
+                    }
+                    oldScrollY=dy;
+                }
+            }
+        });
+*/
+
     }
 
+    public static class NoticeDialogFragment extends DialogFragment{  //public 이 2개있을수없어 static으로 만듬
+       @NonNull
         @Override
-        public void onLongItemClick (View view,int position){
-        Toast.makeText(getApplicationContext(), position + "번 째 아이템 롱 클릭", Toast.LENGTH_SHORT).show();
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+
+           AlertDialog.Builder adBuilder= new AlertDialog.Builder(getActivity());
+           adBuilder.setMessage("Time out!")
+                   .setTitle("Notice")
+                   .setIcon(R.mipmap.ic_launcher)
+                   .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+
+                       }
+                   })
+                   .setCancelable(false);
+           return adBuilder.create();
+           //return super.onCreateDialog(savedInstanceState);
+       }
     }
-    }));
 
 
-}
 }
