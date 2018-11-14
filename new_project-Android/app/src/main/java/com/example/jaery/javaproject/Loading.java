3f647c09b53577048927cecbbfa07aa1152aac7d
jaeryo <jaeryo2357@naver.com>
2018-11-15 00:24:05 +0900

    Logo 대입

--- new_project-Android/app/src/main/java/com/example/jaery/javaproject/Loading.java
+++ new_project-Android/app/src/main/java/com/example/jaery/javaproject/Loading.java
@@ -24,12 +24,24 @@ public class Loading extends AppCompatActivity {
             @Override
             public void run()
             {
-                Intent intent=new Intent(Loading.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                imgAndroid.setAnimation(null);
+
                 //여기에 딜레이 후 시작할 작업들을 입력
+
+                new Handler().postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        Intent intent=new Intent(Loading.this,Register.class);
+                        startActivity(intent);
+                        finish();
+                        //여기에 딜레이 후 시작할 작업들을 입력
+                    }
+                }, 1300);// 3.5초 정도 딜레이를 준 후 시작
+
             }
-        }, 3000);// 0.5초 정도 딜레이를 준 후 시작
+        }, 3900);// 3.5초 정도 딜레이를 준 후 시작
     }
 
     private void initView() {
